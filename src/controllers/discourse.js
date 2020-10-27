06d5d60fb85a88d99f7ad0527877ba2a953f2c91
Rodrigo Torres <ratorres7@gmail.com>
2020-10-27 14:29:51 -0300

    Logging added to many functions

--- src/controllers/discourse.js
+++ src/controllers/discourse.js
@@ -41,6 +41,12 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+fetchQueue.on('active', () => {
+    if (fetchQueue.pending>2){
+        logger.info(`Discourse api queue jobs pending: ${fetchQueue.pending}`)
+    }
+});
+
 exports.config = config;
 
 const fetchDiscordApiQueue = async (path, method, params, body , ntries, expect_json) =>{
@@ -61,7 +67,9 @@ const fetchDiscordApiQueue = async (path, method, params, body , ntries, expect_
         })
     }
 
-    logger.info(`Discourse api ${method} request to "${path}" ${res.status}:${res.statusText} Try number ${ntries+1}`)
+    if ( (path !== 'search') || (res.status !==200 )){
+        logger.info(`Discourse api ${method} request to "${path}" ${res.status}:${res.statusText} Try number ${ntries+1}`)
+    }
 
     if (expect_json){
         let json = await res.json()
@@ -71,6 +79,11 @@ const fetchDiscordApiQueue = async (path, method, params, body , ntries, expect_
 }
 
 const fetchDiscordApi = async (path, method, params={}, body = {}, ntries = 0, expect_json = true) =>{
+    
+        if ( (path !== 'search')){
+            logger.info(`Discourse api ${method} request to "${path}" added to queue`)
+        }
+
         try{
             return await fetchQueue.add( async() => fetchDiscordApiQueue(path, method, params, body, ntries, expect_json)) 
         }
